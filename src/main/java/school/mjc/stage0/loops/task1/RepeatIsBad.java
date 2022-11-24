69d9881b2aabd238fcbe9888aebbc6dc7ba9149b
Artyom7777 <iartiq@gmail.com>
2022-11-24 10:55:40 +0300

    Done

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,10 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        int count = 1;
+        while (count <= 20){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+            count ++;
+        }
     }
 }
